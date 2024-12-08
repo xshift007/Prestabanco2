4de077c5e01d25a31b8529429dac58bd0d00557e
Nicolás Sánchez <nicolas.sanchez.al@usach.cl>
2024-12-08 18:06:24 -0300

    advanced

--- m1-credit-simulation-service/src/main/java/com/example/creditsimulation/M1CreditSimulationServiceApplication.java
+++ m1-credit-simulation-service/src/main/java/com/example/creditsimulation/M1CreditSimulationServiceApplication.java
@@ -1,13 +1,19 @@
-package com.mingeso.m1creditsimulationservice;
+package com.example.creditsimulation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * Clase principal del microservicio de simulación de créditos.
+ * Se habilita EurekaClient para registro futuro en Eureka Server.
+ */
 @SpringBootApplication
 @EnableEurekaClient
 public class M1CreditSimulationServiceApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(M1CreditSimulationServiceApplication.class, args);
 	}
+
 }
