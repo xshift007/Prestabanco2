a836a0477632f06fd2d9330e9059bf9bb6d80f07
Nicolás Sánchez <nicolas.sanchez.al@usach.cl>
2024-12-09 09:26:59 -0300

    morning advanced

--- m3-loan-application-service/src/main/java/com/mingeso/m3loanapplicationservice/repositories/LoanApplicationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mingeso.m3loanapplicationservice.repositories;
-
-import com.mingeso.m3loanapplicationservice.entities.LoanApplication;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoanApplicationRepository extends JpaRepository<LoanApplication, Long> {
-}
