da22db34c9aa5f8dde8ad0db58d059d208ca7ce6
Nicolás Sánchez <nicolas.sanchez.al@usach.cl>
2024-12-11 23:06:18 -0300

    Auto stash before merge of "main" and "origin/main"

--- m3-loan-application-service/src/main/java/com/example/loanapplication/M3LoanApplicationServiceApplication.java
+++ m3-loan-application-service/src/main/java/com/example/loanapplication/M3LoanApplicationServiceApplication.java
@@ -4,14 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-/**
- * Clase principal del microservicio de registro de usuarios.
- */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class M2UserRegistrationServiceApplication {
+public class M3LoanApplicationServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(M2UserRegistrationServiceApplication.class, args);
+		SpringApplication.run(M3LoanApplicationServiceApplication.class, args);
 	}
 }
